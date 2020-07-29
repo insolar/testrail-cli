89e662f54012252e68f695dee4a8ec3da76812fe
Eugine Blikh <bigbes@gmail.com>
2020-07-29 15:05:35 +0300

    NOISSUE: fix installation compatibility
    added stub for installation compatibility between old and new versions of testrail-cli

---
+++ doc.go
@@ -0,0 +1,9 @@
+//  Copyright 2020 Insolar Network Ltd.
+//  All rights reserved.
+//  This material is licensed under the Insolar License version 1.0,
+//  available at https://github.com/insolar/testrail-cli/LICENSE.md.
+
+// stub top-level package to save installation compatibility for
+// old and new versions of testrail-cli
+package testrailcli
+
